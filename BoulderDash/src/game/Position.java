84d7ddcf155da97a19b75744756a7af877ce7c70
Ramiro Sanchez <ramiro@Raniro-PCA.localdomain>
2017-10-13 20:32:56 -0300

    Getter y setters de position y status.

--- BoulderDash/src/game/Position.java
+++ BoulderDash/src/game/Position.java
@@ -3,4 +3,18 @@ package game;
 public class Position {
 	int posX;
 	int posY;
+	
+	public int getPosX() {
+		return posX;
+	}
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
+	public int getPosY() {
+		return posY;
+	}
+	public void setPosY(int posY) {
+		this.posY = posY;
+	}
+	
 }
--- BoulderDash/src/game/Status.java
+++ BoulderDash/src/game/Status.java
@@ -2,4 +2,18 @@ package game;
 
 public class Status {
 	boolean status;
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	
+	public void changeStatus() {
+		if (status) 
+			status=false;
+		else status=true;
+	}
 }
