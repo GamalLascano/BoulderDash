f26f6c249f918174adcf8a79ec5bc228df237075
CorsinoAlexandre <corsinu.alexandre@gmail.com>
2017-10-15 15:50:53 -0300

    Constructor

--- BoulderDash/src/game/item/Diamond.java
+++ BoulderDash/src/game/item/Diamond.java
@@ -1,5 +1,18 @@
 package game.item;
 
+import game.Position;
+import game.Status;
+
 public class Diamond extends Item {
+
+	public Diamond(Status state, Position pos, boolean collectable, boolean moveable, boolean fallable, boolean explodable) {
+		super(state, pos, collectable, moveable, fallable, explodable);
+	}
+	
+	public Diamond(Status state, Position pos) {
+		super(state, pos, true, false, true, false);
+	}
+	
+	
 	
 }
