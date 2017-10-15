d4a9abd6b6f8019c494d67b4a0b5ab7959aa4eab
CorsinoAlexandre <corsinu.alexandre@gmail.com>
2017-10-15 16:28:57 -0300

    metodo open

--- BoulderDash/src/game/cell/Exit.java
+++ BoulderDash/src/game/cell/Exit.java
@@ -1,5 +1,20 @@
 package game.cell;
 
+import game.Position;
+import game.character.Rockford;
+import game.map.BDLevelReader;
+
 public class Exit extends Cell {
 
+	public Exit(Position pos) {
+		super(pos, true, false);
+	}
+
+	public void open(BDLevelReader levelReader, Rockford player ) {
+		if(player.getDiamonds() == levelReader.getDiamondsNeeded()) {
+			this.setActive(true);
+			this.setSolid(false);
+		}
+	}
+	
 }
