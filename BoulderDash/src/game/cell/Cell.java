0f88cad34d5cb9f2fad631a0ea766e4e221a67ab
CorsinoAlexandre <corsinu.alexandre@gmail.com>
2017-10-15 16:10:50 -0300

    parametros, constructor

--- BoulderDash/src/game/cell/Cell.java
+++ BoulderDash/src/game/cell/Cell.java
@@ -2,11 +2,29 @@ package game.cell;
 import game.Status;
 
 public abstract class Cell {
-	Status state;
-	boolean isSolid;
+	boolean active;		//Para la salida y muros magicos.
+	boolean solid;		//Si los personajes pueden caminar sobre la celda.
 	
-	Cell(Status state, boolean isSolid) {
-		this.state = state;
-		this.isSolid = isSolid;
+	Cell(Status state, boolean solid, boolean active) {
+		this.solid = solid;
+		this.active = active;
 	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public boolean isSolid() {
+		return solid;
+	}
+
+	public void setSolid(boolean solid) {
+		this.solid = solid;
+	}
+	
+	
 }
