15c5a150e2f8c7342605af468aa079d27d9afbe6
CorsinoAlexandre <corsinu.alexandre@gmail.com>
2017-10-14 21:11:58 -0300

    posX y posY publico

--- BoulderDash/src/game/Position.java
+++ BoulderDash/src/game/Position.java
@@ -1,18 +1,21 @@
 package game;
 
 public class Position {
-	int posX;
-	int posY;
+	public int posX;
+	public int posY;
 	
 	public int getPosX() {
 		return posX;
 	}
+	
 	public void setPosX(int posX) {
 		this.posX = posX;
 	}
+	
 	public int getPosY() {
 		return posY;
 	}
+	
 	public void setPosY(int posY) {
 		this.posY = posY;
 	}
