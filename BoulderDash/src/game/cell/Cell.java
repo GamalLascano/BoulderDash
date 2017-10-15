9279955894d672241c7a6131a2c59a87932e050f
CorsinoAlexandre <corsinu.alexandre@gmail.com>
2017-10-14 21:15:30 -0300

    Constructor y parametros nuevos

--- BoulderDash/src/game/cell/Cell.java
+++ BoulderDash/src/game/cell/Cell.java
@@ -3,8 +3,12 @@ import game.Status;
 
 public abstract class Cell {
 	Status state;
+	Sprite sprite;
+	boolean isSolid;
 	
-	Cell(Status state) {
+	Cell(Status state, boolean isSolid, Sprite sprite) {
 		this.state = state;
+		this.isSolid = isSolid;
+		this.sprite = sprite;
 	}
 }
