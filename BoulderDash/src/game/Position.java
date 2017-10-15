9f211eb1857d780e0884ef4b1a246c62852070f0
CorsinoAlexandre <corsinu.alexandre@gmail.com>
2017-10-14 21:17:56 -0300

    Constructor nuevo

--- BoulderDash/src/game/Position.java
+++ BoulderDash/src/game/Position.java
@@ -4,6 +4,16 @@ public class Position {
 	public int posX;
 	public int posY;
 	
+	public Position() {
+		super();
+	}
+
+	public Position(int posX, int posY) {
+		super();
+		this.posX = posX;
+		this.posY = posY;
+	}
+
 	public int getPosX() {
 		return posX;
 	}
