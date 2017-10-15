a61d284d83d5b29794bd68fcd8b3bf9a28e3f9b4
CorsinoAlexandre <corsinu.alexandre@gmail.com>
2017-10-14 22:59:22 -0300

    metodos generados, mas status

--- BoulderDash/src/game/Status.java
+++ BoulderDash/src/game/Status.java
@@ -2,8 +2,57 @@ package game;
 
 public class Status {
 	boolean status;
+	boolean exploding;
+	boolean expanding;
+	boolean moving;
+	boolean falling;
 
-	public boolean isStatus() {
+	public Status(){
+		
+	}
+	
+	public Status(boolean status, boolean exploding, boolean expanding, boolean moving, boolean falling) {
+		super();
+		this.status = status;
+		this.exploding = exploding;
+		this.expanding = expanding;
+		this.moving = moving;
+		this.falling = falling;
+	}
+
+	public boolean isExploding() {
+		return exploding;
+	}
+
+	public void setExploding(boolean exploding) {
+		this.exploding = exploding;
+	}
+
+	public boolean isExpanding() {
+		return expanding;
+	}
+
+	public void setExpanding(boolean expanding) {
+		this.expanding = expanding;
+	}
+
+	public boolean isMoving() {
+		return moving;
+	}
+
+	public void setMoving(boolean moving) {
+		this.moving = moving;
+	}
+
+	public boolean isFalling() {
+		return falling;
+	}
+
+	public void setFalling(boolean falling) {
+		this.falling = falling;
+	}
+
+	public boolean getStatus() {
 		return status;
 	}
 
@@ -16,4 +65,11 @@ public class Status {
 			status=false;
 		else status=true;
 	}
+
+	@Override
+	public String toString() {
+		return "Status [status=" + status + ", exploding=" + exploding + ", expanding=" + expanding + ", moving="
+				+ moving + ", falling=" + falling + "]";
+	}
+	
 }
