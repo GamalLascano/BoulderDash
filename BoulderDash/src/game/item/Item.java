fcf4d07bd10a89aafb782c33888145ab127958d9
CorsinoAlexandre <corsinu.alexandre@gmail.com>
2017-10-15 15:15:18 -0300

    Constructor y parametros

--- BoulderDash/src/game/item/Item.java
+++ BoulderDash/src/game/item/Item.java
@@ -2,14 +2,30 @@ package game.item;
 
 import game.Position;
 import game.Status;
+import game.map.BDLevelReader;
+import game.map.BDTile;
 
 public abstract class Item {
 	Status state = new Status(false,false,false,false,false);
 	Sprite sprite;
 	Position pos = new Position();
+	boolean collectable;
+	boolean moveable;
+	boolean explodable;
 	
+	public Item(Status state, Sprite sprite, Position pos, boolean collectable, boolean moveable, boolean explodable) {
+		super();
+		this.state = state;
+		this.sprite = sprite;
+		this.pos = pos;
+		this.collectable = collectable;
+		this.moveable = moveable;
+		this.explodable = explodable;
+	}
+
 	public void fall() {
-		pos.setPosY(pos.getPosY() - 1);
-		state.setFalling(true);
+		while(BDLevelReader.field[pos.posX][pos.posY - 1] == BDTile.EMPTY)
+			pos.setPosY(pos.getPosY() - 1);
+			state.setFalling(true);
 	}
 }
