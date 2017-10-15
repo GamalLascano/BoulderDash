ae47d930e0f6fefba0fec5d72379204189983332
CorsinoAlexandre <corsinu.alexandre@gmail.com>
2017-10-15 15:19:39 -0300

    fix

--- BoulderDash/src/game/item/Item.java
+++ BoulderDash/src/game/item/Item.java
@@ -7,16 +7,14 @@ import game.map.BDTile;
 
 public abstract class Item {
 	Status state = new Status(false,false,false,false,false);
-	Sprite sprite;
 	Position pos = new Position();
 	boolean collectable;
 	boolean moveable;
 	boolean explodable;
 	
-	public Item(Status state, Sprite sprite, Position pos, boolean collectable, boolean moveable, boolean explodable) {
+	public Item(Status state, Position pos, boolean collectable, boolean moveable, boolean explodable) {
 		super();
 		this.state = state;
-		this.sprite = sprite;
 		this.pos = pos;
 		this.collectable = collectable;
 		this.moveable = moveable;
