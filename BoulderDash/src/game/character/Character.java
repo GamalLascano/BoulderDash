6afa712ce55daef72a07b00a92b4de76ab0d46ce
CorsinoAlexandre <corsinu.alexandre@gmail.com>
2017-10-14 21:19:50 -0300

    Metodo move hecho

--- BoulderDash/src/game/character/Character.java
+++ BoulderDash/src/game/character/Character.java
@@ -1,8 +1,36 @@
 package game.character;
+import game.CurrentDirection;
+import game.Position;
 
 public abstract class Character {
+	Position coordenada = new Position();
 	
-	protected abstract boolean explode();
-	protected abstract boolean move();
+	/**
+	 * Movimiento en el mapa, poner una direcion como parametro.
+	 * usa game.Position y game.CurrentDirection.
+	 */
+	public boolean move(CurrentDirection direction) {
+		switch (direction) {
+			case UP :
+				coordenada.setPosY(coordenada.getPosY() + 1);
+				break;
+			case DOWN :
+				coordenada.setPosY(coordenada.getPosY() - 1);
+				break;
+			case LEFT :
+				coordenada.setPosX(coordenada.getPosX() - 1);
+				break;
+			case RIGHT :
+				coordenada.setPosX(coordenada.getPosX() + 1);
+				break;
+			default :
+				break;
+		}
+		return true;
+	}
+	
+	public boolean explode() {
+		return true;
+	}
 	
 }
