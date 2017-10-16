4f5a0af39ca2c86acdbfc3a654fd4ce982a0165a
CorsinoAlexandre <corsinu.alexandre@gmail.com>
2017-10-15 23:50:10 -0300

    movement

--- BoulderDash/src/game/item/Rock.java
+++ BoulderDash/src/game/item/Rock.java
@@ -1,9 +1,28 @@
 package game.item;
 
+import game.Position;
+import game.Status;
+import game.character.Rockford;
+
 public class Rock extends Item {
 
-	private boolean movement() {
-		 return true;
+	
+	
+	public Rock(Status state, Position pos)
+	{
+		super(state, pos, false, true, true, false);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * Rockford empuja la roca. direciones?
+	 * @return
+	 */
+	private void movement(Rockford player) {
+		if (player.isPushing() && this.moveable) {
+			this.pos.setPosX(this.pos.posX++);
+			this.state.setMoving(true);
+		}
 	}
 	
 	private boolean checkEmpty() {
