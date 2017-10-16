42a921b4f23e69c8e7a5e2e4eba23f6e548d6283
CorsinoAlexandre <corsinu.alexandre@gmail.com>
2017-10-15 23:45:41 -0300

    Empuje

--- BoulderDash/src/game/character/Rockford.java
+++ BoulderDash/src/game/character/Rockford.java
@@ -2,13 +2,16 @@ package game.character;
 
 import game.Position;
 import game.Status;
+import game.item.Item;
 
 public class Rockford extends Character {
 	int score;
 	int diamonds;
-	
-	public Rockford(Status state, Position pos) {
-		super(state, pos, true);
+	boolean pushing;
+
+	public Rockford(Status state, Position pos)
+	{
+		super(state, pos);
 		// TODO Auto-generated constructor stub
 	}
 
@@ -28,8 +31,26 @@ public class Rockford extends Character {
 		this.diamonds = diamonds;
 	}
 
+	
+	public boolean isPushing()
+	{
+		return pushing;
+	}
+
+	public void setPushing(boolean pushing)
+	{
+		this.pushing = pushing;
+	}
+
 	public boolean save() {
 		return true;
+	}
+	
+	//hacer una clase proximidad? sino hay que hacer empujar en cada direction
+	public void push(Item item) {
+		while ( this.pos.getPosX() == item.getPos().getPosX() + 1) {
+			this.pushing = true;
+		}
 	}
 	
 	//singleton aca??
