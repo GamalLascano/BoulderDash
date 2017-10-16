9f2c73a879a5f69c3d333192e8c2b05dc5d1e90d
CorsinoAlexandre <corsinu.alexandre@gmail.com>
2017-10-16 00:08:38 -0300

    Constructor

--- BoulderDash/src/game/item/Dirt.java
+++ BoulderDash/src/game/item/Dirt.java
@@ -1,6 +1,15 @@
 package game.item;
 
+import game.Position;
+import game.Status;
+
 public class Dirt extends Item {
 
+	public Dirt(Status state, Position pos) {
+		super(state, pos, true, false, false, false);
+		// TODO Auto-generated constructor stub
+	}
+
+	
 	
 }
