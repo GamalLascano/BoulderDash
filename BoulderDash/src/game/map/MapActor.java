0cee67c8194940b1507d9cb90f876423100b8b9d
Ramiro Sanchez <ramiro_sanchez@outlook.com.ar>
2017-10-18 17:10:56 -0300

    MapActor

--- BoulderDash/src/game/map/MapActor.java
+++ BoulderDash/src/game/map/MapActor.java
@@ -1,5 +1,36 @@
 package game.map;
 import game.actor.*;
+import game.Position;
+
 public class MapActor {
+	private Actor[][] actor;
+	private int width;
+	private int height;
+	
+	public MapActor(Actor[][] actor, int width, int height) {
+		this.actor = new Actor[width][height];
+		this.width = width;
+		this.height = height;
+	}
+	
+	public Actor getActor(Position pos) {
+		return actor[pos.getPosX()][pos.getPosY()];
+	}
+	
+	/**
+	 * 
+	 * @param pos
+	 * @param act
+	 * @return : true si se agrego correctamente
+	 */
+	public boolean setActor(Position pos, Actor act) {
+		if(this.width < pos.getPosX() && this.height < pos.getPosY()) {
+			actor[pos.getPosX()][pos.getPosY()] = act;
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 	
 }
