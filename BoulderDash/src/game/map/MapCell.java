db1e67d7af4eff61601c7eb2c4b623291d7584d2
Ramiro Sanchez <ramiro_sanchez@outlook.com.ar>
2017-10-18 17:17:43 -0300

    MapCell

--- BoulderDash/src/game/map/MapCell.java
+++ BoulderDash/src/game/map/MapCell.java
@@ -1,5 +1,35 @@
 package game.map;
+import game.cell.*;
+import game.Position;
 
 public class MapCell {
-
+	private Cell[][] cell;
+	private int width;
+	private int height;
+	
+	public MapCell(Cell[][] cell, int width, int height) {
+		this.cell = new Cell[width][height];
+		this.width = width;
+		this.height = height;
+	}
+	
+	public Cell getActor(Position pos) {
+		return cell[pos.getPosX()][pos.getPosY()];
+	}
+	
+	/**
+	 * 
+	 * @param pos
+	 * @param act
+	 * @return : true si se agrego correctamente
+	 */
+	public boolean setActor(Position pos, Cell act) {
+		if(this.width < pos.getPosX() && this.height < pos.getPosY()) {
+			cell[pos.getPosX()][pos.getPosY()] = act;
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 }
