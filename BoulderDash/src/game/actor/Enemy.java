926a733b0ebf44b165a23c3ba69c4bbc663c02dd
Ramiro Sanchez <ramiro_sanchez@outlook.com.ar>
2017-10-19 17:07:01 -0300

    Add Explode

--- BoulderDash/src/game/actor/Enemy.java
+++ BoulderDash/src/game/actor/Enemy.java
@@ -11,5 +11,20 @@ public abstract class Enemy extends Actor {
 	}
 
 	protected abstract boolean rotate();
+	
+	public void explode() {
+		//Hay bloques que no pueden desparecer! ej: bloque salida
+		//Hay que ver como tratar con las instancias de las cells
+		MapInstance.loadData(BDTile.EMPTY, pos.getPosX(), pos.getPosY());
+		MapInstance.loadData(BDTile.EMPTY, pos.getPosX() + 1, pos.getPosY());
+		MapInstance.loadData(BDTile.EMPTY, pos.getPosX()-1, pos.getPosY());
+		MapInstance.loadData(BDTile.EMPTY, pos.getPosX(), pos.getPosY() + 1);
+		MapInstance.loadData(BDTile.EMPTY, pos.getPosX(), pos.getPosY() - 1);
+		MapInstance.loadData(BDTile.EMPTY, pos.getPosX() + 1, pos.getPosY() + 1);
+		MapInstance.loadData(BDTile.EMPTY, pos.getPosX() - 1, pos.getPosY() - 1);
+		MapInstance.loadData(BDTile.EMPTY, pos.getPosX() - 1, pos.getPosY() + 1);
+		MapInstance.loadData(BDTile.EMPTY, pos.getPosX() + 1, pos.getPosY() - 1);
+		
+	}
 
 }
