c37c7d3c97d31516a9e8f4b766288cd193b497a1
CorsinoAlexandre <corsinu.alexandre@gmail.com>
2017-10-20 17:17:35 -0300

    listas

--- BoulderDash/src/game/map/ListActor.java
+++ BoulderDash/src/game/map/ListActor.java
@@ -5,6 +5,23 @@ import java.util.List;
 
 public class ListActor<Actor>
 {
-	public List<Actor> ac = new ArrayList<Actor>();
+	private List<Actor> ac = new ArrayList<Actor>();
+
+	public ListActor(List<Actor> ac)
+	{
+		super();
+		this.ac = ac;
+	}
+
+	public List<Actor> getAc()
+	{
+		return ac;
+	}
+
+	public void setAc(List<Actor> ac)
+	{
+		this.ac = ac;
+	}
 	
+	 
 }
--- BoulderDash/src/game/map/ListItem.java
+++ BoulderDash/src/game/map/ListItem.java
@@ -1,6 +1,28 @@
 package game.map;
 
+import game.item.Item;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListItem
 {
+	private List<Item> it = new ArrayList<Item>();
 
+	public ListItem(List<Item> it)
+	{
+		super();
+		this.it = it;
+	}
+
+	public List<Item> getIt()
+	{
+		return it;
+	}
+
+	public void setIt(List<Item> it)
+	{
+		this.it = it;
+	}
+	
+	
 }
