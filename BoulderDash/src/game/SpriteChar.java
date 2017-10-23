1fefd3fbf6aa0052bc316186c43a8221c60a696a
GamalLascano <gamallascano@gmail.com>
2017-10-23 17:36:55 -0300

    -comment

--- BoulderDash/src/game/SpriteChar.java
+++ BoulderDash/src/game/SpriteChar.java
@@ -4,7 +4,15 @@ package game;
  * Este enum contiene la representacion grafica de todas las entidades del juego
  * R:Rockford
  * F:Firefly
- * B:bla
+ * B:Butterfly
+ * D:Dirt
+ * T:Titanium
+ * W:Wall
+ * S:Exit
+ * O:Rock
+ * X:Diamond
+ * A:Ameoba
+ * E:Empty
  *
  */
 public enum SpriteChar
