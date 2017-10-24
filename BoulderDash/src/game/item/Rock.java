d92e840801a9fbb7214abd95f5652d440fff7ef3
GamalLascano <gamallascano@gmail.com>
2017-10-23 21:04:02 -0300

    -comments

--- BoulderDash/src/game/item/Rock.java
+++ BoulderDash/src/game/item/Rock.java
@@ -4,20 +4,29 @@ import game.Position;
 import game.SpriteChar;
 import game.actor.Rockford;
 
+/**Esta clase representa una roca, junto con su caracter de representacion y un booleano
+ * para ver si esta siendo empujado o no, junto con otras propiedades de otros items
+ */
 public class Rock extends Item
 {
 	SpriteChar spritechar = SpriteChar.O;
 	boolean pushed;
 
+	/** Permite inicializar a la roca con un estado de movimiento y una posicion predeterminada
+	 * @param state: estado de movimiento
+	 * @param pos: posicion
+	 */
 	public Rock(StatusItem state, Position pos)
 	{
 		super(state, pos, false, true, true, false, true, true);
 		this.pushed = false;
-		// TODO Auto-generated constructor stub
 	}
 
 	// GETTERS
 	
+	/** Permite obtener al caracter de una roca
+	 * 
+	 */
 	public SpriteChar getSpritechar()
 	{
 		return spritechar;
@@ -25,12 +34,10 @@ public class Rock extends Item
 	
 	// MOVIMIENTO
 	
-	/**
-	 * Rockford empuja la roca. direciones?
-	 * 
-	 * @return
+
+	/** Permite modificar el estado de la roca si esta siendo pusheada por rockford
+	 * @param player: El objeto que representa a rockford
 	 */
-	
 	public void pushed(Rockford player)
 	{
 		if (player.isPushing() && this.moveable)
