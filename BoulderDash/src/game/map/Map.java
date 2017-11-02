0dabd15364c8e2c5b990b255cdbc614fabb1a539
CorsinoAlexandre <corsinu.alexandre@gmail.com>
2017-11-02 16:33:18 -0300

    format

--- BoulderDash/src/game/map/Map.java
+++ BoulderDash/src/game/map/Map.java
@@ -1,29 +1,33 @@
 package game.map;
+
 import game.map.bdlevel.*;
 import game.Position;
+
 public abstract class Map
 {
 	private static Map singleton;
 	private static BDLevelReader level;
 	private static Object[][] matrix;
-	
-	public Map() {
-		matrix=null;
+
+	public Map()
+	{
+		matrix = null;
 	}
-	
+
 	abstract Map getInstance();
-	
+
 	abstract void start(BDLevelReader level);
-	
+
 	abstract Object getObject(Position pos);
-	
+
 	abstract Object getObject(Integer x, Integer y);
-	
+
 	abstract boolean setObject(Position pos, Object obj);
-	
+
 	abstract boolean removeObject(Position pos);
-	
-	public void fill() {
+
+	public void fill()
+	{
 		for (int x = 0; x < level.getWIDTH(); x++)
 			for (int y = 0; y < level.getHEIGHT(); y++)
 			{
--- BoulderDash/src/game/map/MapInstance.java
+++ BoulderDash/src/game/map/MapInstance.java
@@ -8,8 +8,10 @@ import game.Entity;
 import game.map.bdlevel.BDLevelReader;
 import game.Position;
 
-/** Esta clase contiene la instancia del mapa principal del juego, y controla la gran parte del movimiento del juego
- *  Es singleton, y contiene a un mapa de celdas, items y actores, junto con una lista con todas las entidades vivas
+/**
+ * Esta clase contiene la instancia del mapa principal del juego, y controla la
+ * gran parte del movimiento del juego Es singleton, y contiene a un mapa de
+ * celdas, items y actores, junto con una lista con todas las entidades vivas
  */
 public class MapInstance
 {
@@ -31,12 +33,14 @@ public class MapInstance
 
 	// SINGLETON
 
-	/** Devuelve la unica instancia del mapa
+	/**
+	 * Devuelve la unica instancia del mapa
+	 * 
 	 * @return el singleton de la clase
 	 */
 	public static synchronized MapInstance getInstance()
 	{
-		//Si la instancia no se creo, se crea, y se devuelve la instancia
+		// Si la instancia no se creo, se crea, y se devuelve la instancia
 		if (singleton == null)
 		{
 			singleton = new MapInstance();
@@ -67,13 +71,16 @@ public class MapInstance
 	}
 
 	// SETTERS
-	
 
 	// INICIALIZACION
-	
-	/** Con este metodo, creo cada uno de los mapas para que se puedan usar independientemente
-	 *  En conjunto con una lista con todas las entidades que se mueven
-	 * @param levels: El lector de niveles del juego
+
+	/**
+	 * Con este metodo, creo cada uno de los mapas para que se puedan usar
+	 * independientemente En conjunto con una lista con todas las entidades que
+	 * se mueven
+	 * 
+	 * @param levels:
+	 *            El lector de niveles del juego
 	 */
 	public static void start(BDLevelReader levels)
 	{
@@ -86,25 +93,28 @@ public class MapInstance
 		entitiesAlive = ActiveEntities.getInstance();
 		ActiveEntities.start();
 	}
-	
+
 	// ACTUALIZAR POSICION
 
 	/**
-	 * Se occupa de mover a Rockford en la matriz, tambien verifica si la celda destino es solida para moverse.
-	 * Rockford cava automaticamente la tierra.
+	 * Se occupa de mover a Rockford en la matriz, tambien verifica si la celda
+	 * destino es solida para moverse. Rockford cava automaticamente la tierra.
+	 * 
 	 * @param player
 	 */
 	private static void movingRockford(Rockford player)
 	{
-		//En el cado de moverse arriba, abajo, izquierda o derecha, se mueve de forma diferente
-		switch ( player.getState().getStateEnum() )
+		// En el cado de moverse arriba, abajo, izquierda o derecha, se mueve de
+		// forma diferente
+		switch (player.getState().getStateEnum())
 		{
 			case MOVINGUP:
-				//Si las celdas e items de arriba de rockford no son solidos...
-				if( cellMap.getCell(player.getPosition().getX(), player.getPosition().checkUp()).isSolid() < 2
+				// Si las celdas e items de arriba de rockford no son solidos...
+				if (cellMap.getCell(player.getPosition().getX(), player.getPosition().checkUp()).isSolid() < 2
 						&& itemMap.getItem(player.getPosition().getX(), player.getPosition().checkUp()).isSolid() < 2)
 				{
-					//..rompo la tierra arriba de rockford y agarro el diamante en el caso de que haya
+					// ..rompo la tierra arriba de rockford y agarro el diamante
+					// en el caso de que haya
 					player.getPosition().goUp();
 					player.dig(cellMap.getDirt(player.getPosition()));
 					player.collect(itemMap.getDiamond(player.getPosition()));
@@ -115,11 +125,12 @@ public class MapInstance
 				}
 				break;
 			case MOVINGDOWN:
-				//Si las celdas e items de abajo de rockford no son solidos...
-				if( cellMap.getCell(player.getPosition().getX(), player.getPosition().checkDown()).isSolid() < 2
+				// Si las celdas e items de abajo de rockford no son solidos...
+				if (cellMap.getCell(player.getPosition().getX(), player.getPosition().checkDown()).isSolid() < 2
 						&& itemMap.getItem(player.getPosition().getX(), player.getPosition().checkDown()).isSolid() < 2)
 				{
-					//..rompo la tierra abajo de rockford y agarro el diamante en el caso de que haya
+					// ..rompo la tierra abajo de rockford y agarro el diamante
+					// en el caso de que haya
 					player.getPosition().goDown();
 					player.dig(cellMap.getDirt(player.getPosition()));
 					player.collect(itemMap.getDiamond(player.getPosition()));
@@ -130,22 +141,28 @@ public class MapInstance
 				}
 				break;
 			case MOVINGRIGHT:
-				//Si las celdas e items en la derecha de rockford no son solidos...
-				if( cellMap.getCell(player.getPosition().checkRight(), player.getPosition().getY()).isSolid() < 2
-						&& itemMap.getItem(player.getPosition().checkRight(), player.getPosition().getY()).isSolid() < 2)
+				// Si las celdas e items en la derecha de rockford no son
+				// solidos...
+				if (cellMap.getCell(player.getPosition().checkRight(), player.getPosition().getY()).isSolid() < 2
+						&& itemMap.getItem(player.getPosition().checkRight(), player.getPosition().getY())
+								.isSolid() < 2)
 				{
-					//..rompo la tierra en la derecha de rockford y agarro el diamante en el caso de que haya
+					// ..rompo la tierra en la derecha de rockford y agarro el
+					// diamante en el caso de que haya
 					player.getPosition().goRight();
 					player.dig(cellMap.getDirt(player.getPosition()));
 					player.collect(itemMap.getDiamond(player.getPosition()));
 				}
-				//Si no es solido, es movible, y no hay tierra al lado.
-				else if( cellMap.getCell(player.getPosition().checkRight(), player.getPosition().getY()).isSolid() < 2
-						&& itemMap.getItem(player.getPosition().checkRight(), player.getPosition().getY()).isMoveable() == true
-						&& itemMap.getItem(player.getPosition().checkRight() + 1, player.getPosition().getY()).isSolid() < 1
-						&& cellMap.getCell(player.getPosition().checkRight() + 1, player.getPosition().getY()).isSolid() < 1)
+				// Si no es solido, es movible, y no hay tierra al lado.
+				else if (cellMap.getCell(player.getPosition().checkRight(), player.getPosition().getY()).isSolid() < 2
+						&& itemMap.getItem(player.getPosition().checkRight(), player.getPosition().getY())
+								.isMoveable() == true
+						&& itemMap.getItem(player.getPosition().checkRight() + 1, player.getPosition().getY())
+								.isSolid() < 1
+						&& cellMap.getCell(player.getPosition().checkRight() + 1, player.getPosition().getY())
+								.isSolid() < 1)
 				{
-					//Se pushea lo que haya
+					// Se pushea lo que haya
 					player.getPosition().goRight();
 					player.dig(cellMap.getDirt(player.getPosition()));
 					player.collect(itemMap.getDiamond(player.getPosition()));
@@ -157,22 +174,27 @@ public class MapInstance
 				}
 				break;
 			case MOVINGLEFT:
-				//Si las celdas e items en la izquierda de rockford no son solidos...
-				if( cellMap.getCell(player.getPosition().checkLeft(), player.getPosition().getY()).isSolid() < 2
+				// Si las celdas e items en la izquierda de rockford no son
+				// solidos...
+				if (cellMap.getCell(player.getPosition().checkLeft(), player.getPosition().getY()).isSolid() < 2
 						&& itemMap.getItem(player.getPosition().checkLeft(), player.getPosition().getY()).isSolid() < 2)
 				{
-					//..rompo la tierra en la derecha de rockford y agarro el diamante en el caso de que haya
+					// ..rompo la tierra en la derecha de rockford y agarro el
+					// diamante en el caso de que haya
 					player.getPosition().goLeft();
 					player.dig(cellMap.getDirt(player.getPosition()));
 					player.collect(itemMap.getDiamond(player.getPosition()));
 				}
-				//Si no es solido, es movible, y no hay tierra al lado
-				else if( cellMap.getCell(player.getPosition().checkLeft(), player.getPosition().getY()).isSolid() < 2
-						&& itemMap.getItem(player.getPosition().checkLeft(), player.getPosition().getY()).isMoveable() == true
-						&& itemMap.getItem(player.getPosition().checkLeft() + 1, player.getPosition().getY()).isSolid() < 1
-						&& cellMap.getCell(player.getPosition().checkLeft() + 1, player.getPosition().getY()).isSolid() < 1)
+				// Si no es solido, es movible, y no hay tierra al lado
+				else if (cellMap.getCell(player.getPosition().checkLeft(), player.getPosition().getY()).isSolid() < 2
+						&& itemMap.getItem(player.getPosition().checkLeft(), player.getPosition().getY())
+								.isMoveable() == true
+						&& itemMap.getItem(player.getPosition().checkLeft() + 1, player.getPosition().getY())
+								.isSolid() < 1
+						&& cellMap.getCell(player.getPosition().checkLeft() + 1, player.getPosition().getY())
+								.isSolid() < 1)
 				{
-					//Se pushea lo que haya
+					// Se pushea lo que haya
 					player.getPosition().goLeft();
 					player.dig(cellMap.getDirt(player.getPosition()));
 					player.collect(itemMap.getDiamond(player.getPosition()));
@@ -187,64 +209,66 @@ public class MapInstance
 				player.collect(itemMap.getDiamond(player.getPosition()));
 				break;
 		}
-		//Por ultimo se setea al jugador en idle
+		// Por ultimo se setea al jugador en idle
 		player.getState().setStateEnum(StatusActorEnum.IDLE);
 	}
-	
+
 	/**
 	 * Se occupa de hacer mover a los enemigos.
-	 * @param actor: El actor que se necesita mover
+	 * 
+	 * @param actor:
+	 *            El actor que se necesita mover
 	 */
 	private static void movingEnemy(Enemy enemy)
 	{
-		switch ( enemy.getState().getStateEnum() )
+		switch (enemy.getState().getStateEnum())
 		{
 			case MOVINGUP:
-				if( cellMap.getCell(enemy.getPosition().getX(), enemy.getPosition().checkUp()).isSolid() < 1
-				&& itemMap.getItem(enemy.getPosition().getX(), enemy.getPosition().checkUp()).isSolid() < 1)
+				if (cellMap.getCell(enemy.getPosition().getX(), enemy.getPosition().checkUp()).isSolid() < 1
+						&& itemMap.getItem(enemy.getPosition().getX(), enemy.getPosition().checkUp()).isSolid() < 1)
 				{
 					enemy.getPosition().goUp();
 				}
 				else
 				{
 					((Firefly) enemy).rotate();
-					//((Butterfly) enemy).rotate();
+					// ((Butterfly) enemy).rotate();
 				}
 				break;
 			case MOVINGDOWN:
-				if( cellMap.getCell(enemy.getPosition().getX(), enemy.getPosition().checkDown()).isSolid() < 1
-				&& itemMap.getItem(enemy.getPosition().getX(), enemy.getPosition().checkDown()).isSolid() < 1)
+				if (cellMap.getCell(enemy.getPosition().getX(), enemy.getPosition().checkDown()).isSolid() < 1
+						&& itemMap.getItem(enemy.getPosition().getX(), enemy.getPosition().checkDown()).isSolid() < 1)
 				{
 					enemy.getPosition().goDown();
 				}
 				else
 				{
 					((Firefly) enemy).rotate();
-					//((Butterfly) enemy).rotate();
+					// ((Butterfly) enemy).rotate();
 				}
 				break;
 			case MOVINGRIGHT:
-				if( cellMap.getCell(enemy.getPosition().checkRight(), enemy.getPosition().getY()).isSolid() < 1
-				&& itemMap.getItem(enemy.getPosition().checkRight(), enemy.getPosition().getY()).isSolid() < 1)
+				if (cellMap.getCell(enemy.getPosition().checkRight(), enemy.getPosition().getY()).isSolid() < 1
+						&& itemMap.getItem(enemy.getPosition().checkRight(), enemy.getPosition().getY()).isSolid() < 1)
 				{
 					enemy.getPosition().goRight();
 				}
 				else
 				{
 					((Firefly) enemy).rotate();
-					//((Butterfly) enemy).rotate();
+					// ((Butterfly) enemy).rotate();
 				}
 				break;
 			case MOVINGLEFT:
-				if( cellMap.getCell(enemy.getPosition().checkLeft(), enemy.getPosition().getY()).isSolid() < 1
-				&& itemMap.getItem(enemy.getPosition().checkLeft(), enemy.getPosition().getY()).isSolid() < 1)
+				if (cellMap.getCell(enemy.getPosition().checkLeft(), enemy.getPosition().getY()).isSolid() < 1
+						&& itemMap.getItem(enemy.getPosition().checkLeft(), enemy.getPosition().getY()).isSolid() < 1)
 				{
 					enemy.getPosition().goLeft();
 				}
 				else
 				{
 					((Firefly) enemy).rotate();
-					//((Butterfly) enemy).rotate();
+					// ((Butterfly) enemy).rotate();
 				}
 				break;
 			default:
@@ -255,6 +279,7 @@ public class MapInstance
 
 	/**
 	 * Se occupa de hacer caer a los objetos en la matriz
+	 * 
 	 * @param item
 	 */
 	private static void fallingItem(Item item)
@@ -263,25 +288,25 @@ public class MapInstance
 		switch (status)
 		{
 			case FALLING:
-				if( cellMap.getCell(item.getPosition().getX(), item.getPosition().checkDown()).isSolid() < 1
-				&& itemMap.getItem(item.getPosition().getX(), item.getPosition().checkDown()).isSolid() < 1
-				&& actorMap.getActor(item.getPosition().getX(), item.getPosition().checkDown()) != null)
+				if (cellMap.getCell(item.getPosition().getX(), item.getPosition().checkDown()).isSolid() < 1
+						&& itemMap.getItem(item.getPosition().getX(), item.getPosition().checkDown()).isSolid() < 1
+						&& actorMap.getActor(item.getPosition().getX(), item.getPosition().checkDown()) != null)
 				{
 					item.getPosition().goDown();
 				}
 				break;
 			case SLIDINGRIGHT:
-				if( cellMap.getCell(item.getPosition().checkRight(), item.getPosition().getY()).isSolid() < 1
-				&& itemMap.getItem(item.getPosition().checkRight(), item.getPosition().getY()).isSolid() < 1
-				&& actorMap.getActor(item.getPosition().checkRight(), item.getPosition().getY()) != null)
+				if (cellMap.getCell(item.getPosition().checkRight(), item.getPosition().getY()).isSolid() < 1
+						&& itemMap.getItem(item.getPosition().checkRight(), item.getPosition().getY()).isSolid() < 1
+						&& actorMap.getActor(item.getPosition().checkRight(), item.getPosition().getY()) != null)
 				{
 					item.getPosition().goRight();
 				}
 				break;
 			case SLIDINGLEFT:
-				if( cellMap.getCell(item.getPosition().checkLeft(), item.getPosition().getY()).isSolid() < 1
-				&& itemMap.getItem(item.getPosition().checkLeft(), item.getPosition().getY()).isSolid() < 1
-				&& actorMap.getActor(item.getPosition().checkLeft(), item.getPosition().getY()) != null)
+				if (cellMap.getCell(item.getPosition().checkLeft(), item.getPosition().getY()).isSolid() < 1
+						&& itemMap.getItem(item.getPosition().checkLeft(), item.getPosition().getY()).isSolid() < 1
+						&& actorMap.getActor(item.getPosition().checkLeft(), item.getPosition().getY()) != null)
 				{
 					item.getPosition().goLeft();
 				}
@@ -294,25 +319,28 @@ public class MapInstance
 
 	/**
 	 * Actualiza la posicion de la entidad en la matriz.
+	 * 
 	 * @param entity
 	 */
 	public static void changePosition(Entity entity)
 	{
-		//Si esa entidad es rockford, se mueve y se hacen las operaciones determinadas
+		// Si esa entidad es rockford, se mueve y se hacen las operaciones
+		// determinadas
 		if (entity instanceof Rockford)
 		{
 			actorMap.removeActor(entity.getPosition());
 			movingRockford((Rockford) entity);
 			actorMap.setActor(entity.getPosition(), (Rockford) entity);
 		}
-		//Si esa entidad es otro actor, se hace lo mismo
+		// Si esa entidad es otro actor, se hace lo mismo
 		else if (entity instanceof Actor)
 		{
 			actorMap.removeActor(entity.getPosition());
 			movingEnemy((Enemy) entity);
 			actorMap.setActor(entity.getPosition(), (Actor) entity);
 		}
-		//Si esa entidad es otro item, se hace lo mismo, pero si es un item que se cae, se checkea si cae
+		// Si esa entidad es otro item, se hace lo mismo, pero si es un item que
+		// se cae, se checkea si cae
 		else if (entity instanceof Item)
 		{
 			itemMap.removeItem(entity.getPosition());
@@ -325,7 +353,8 @@ public class MapInstance
 	// TURNOS
 
 	/**
-	 * Actualiza el mapa. Actualiza la posicion de todas las entidades en la matriz.
+	 * Actualiza el mapa. Actualiza la posicion de todas las entidades en la
+	 * matriz.
 	 */
 	public static void refresh()
 	{
@@ -341,26 +370,30 @@ public class MapInstance
 
 	/**
 	 * Construye el mapa. Convierte la matriz tiles a 3 matrices. 1 matriz de
-	 * actores, 1 matriz de items, 1 matriz de celdas. Agrega actores y items
-	 * en una lista de entities.
-	 * @param level : nivel.
+	 * actores, 1 matriz de items, 1 matriz de celdas. Agrega actores y items en
+	 * una lista de entities.
+	 * 
+	 * @param level
+	 *            : nivel.
 	 */
 	public static void buildMap(BDLevelReader level)
 	{
-		//Para armar el mapa, voy por todo el nivel
+		// Para armar el mapa, voy por todo el nivel
 		for (int y = 0; y < level.getHEIGHT(); y++)
 		{
 			for (int x = 0; x < level.getWIDTH(); x++)
 			{
-				//Hago un nuevo status para el item/actor nuevo, y uso la posicion actual
+				// Hago un nuevo status para el item/actor nuevo, y uso la
+				// posicion actual
 				StatusItem stateItem = new StatusItem();
 				StatusActor stateActor = new StatusActor();
 				Position pos = new Position();
-				
+
 				pos.setXY(x, y);
 				stateItem.reset(StatusItemEnum.IDLE, true);
 				stateActor.reset(StatusActorEnum.IDLE, true);
-				//y dependiendo de lo que se encuentre, se guarda en cada uno de los mapas
+				// y dependiendo de lo que se encuentre, se guarda en cada uno
+				// de los mapas
 				switch (level.getTile(x, y))
 				{
 					case EMPTY:
