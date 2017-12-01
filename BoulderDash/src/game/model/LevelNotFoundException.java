dd37fb4265b06c30228dd96cb8dcad5b32a3038b
CorsinoAlexandre <corsinu.alexandre@gmail.com>
2017-11-30 21:13:46 -0300

    Excepciones

---
+++ BoulderDash/src/game/model/LevelNotFoundException.java
@@ -0,0 +1,29 @@
+package game.model;
+
+public class LevelNotFoundException extends Exception
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public LevelNotFoundException()
+	{
+		super();
+	}
+
+	public LevelNotFoundException(String message)
+	{
+		super(message);
+	}
+
+	public LevelNotFoundException(String message, Throwable cause)
+	{
+		super(message, cause);
+	}
+
+	public LevelNotFoundException(Throwable cause)
+	{
+		super(cause);
+	}
+}
---
+++ BoulderDash/src/game/model/RockfordNotInLevelException.java
@@ -0,0 +1,12 @@
+package game.model;
+
+public class RockfordNotInLevelException extends RuntimeException
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+}
